6b323bb13587b42c059e4d8b68c79b7ddc289ad6
cpriyadarshini <cpriyadarshini@outsetmedical.com>
2018-03-20 06:37:54 -0700

    Add doc comment in movie.py

--- movie.py
+++ movie.py
@@ -3,6 +3,15 @@ import webbrowser
 class Movie():
 
 	def __init__(self, movieTitle, movieStoryLine, movieLink, movieTrailer, moviePoster):
+		"""This is a example showing docstrings
+    
+    Attributes:
+        title (str): Title of the movie
+        story (str): Story of the movie
+        link (str): Link containing more details about the movie
+        trailer(str) : You tube trailer ID
+    	poster(str) : Link to the poster of the movie
+    """
 		self.title = movieTitle;
 		self.story = movieStoryLine;
 		self.link = movieLink;
